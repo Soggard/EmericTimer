afb2d6b4586331ab528f8a91e57f759cad8a24d6
Soggard <e.bernet@laposte.net>
2018-01-19 18:15:17 +0100

    wololoing sound and priest

--- app/src/main/java/net/emericbr/emerictimer/MainActivity.kt
+++ app/src/main/java/net/emericbr/emerictimer/MainActivity.kt
@@ -1,18 +1,21 @@
 package net.emericbr.emerictimer
 
 import android.annotation.SuppressLint
+import android.media.MediaPlayer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.util.Log
 import android.widget.SeekBar
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.image
 
 class MainActivity : AppCompatActivity() {
 
     private var isCountDownActive = false
     private lateinit var countDownTimer: CountDownTimer
     private var currentSeekBarPosition = 0
+    private lateinit var mediaPlayer: MediaPlayer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,6 +39,9 @@ class MainActivity : AppCompatActivity() {
 
         btnTimer.setOnClickListener { toggleTimer() }
 
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.wololo);
+
         seekBar.max = 5 * 60
     }
 
@@ -52,11 +58,14 @@ class MainActivity : AppCompatActivity() {
 
     fun toggleTimer() {
         if (!isCountDownActive) {
+            imageView.setImageDrawable(resources.getDrawable(R.drawable.blue_priest))
             btnTimer.text = getString(R.string.reset)
             currentSeekBarPosition = seekBar.progress
             countDownTimer = object: CountDownTimer((seekBar.progress * 1000).toLong() + 100, 1000){
                 override fun onFinish() {
                     // WOLOLO
+                    mediaPlayer.start()
+                    imageView.setImageDrawable(resources.getDrawable(R.drawable.red_priest))
                     resetTimer()
                 }
 
@@ -85,6 +94,8 @@ class MainActivity : AppCompatActivity() {
 
         updateTimer(currentSeekBarPosition, false)
     }
+
+
 
     companion object {
         internal const val TAG = "MainActivity"
